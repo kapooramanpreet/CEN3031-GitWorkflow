b257979b1d7eb05955bfe77f369161b52624c589
Matthew Silversmith <msilversmith@ufl.edu>
2017-09-19 15:29:00 -0400

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -6,19 +6,25 @@ function add(){
 
 function subtract(){
 	//Implement this
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1-v2).toString());
 	
 }
 
 function multiply(){
 	//Implement this
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1*v2).toString());
 	
 }
 
 function divide(){
 	//Implement this
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1/v2).toString());
 	
 	
 }
