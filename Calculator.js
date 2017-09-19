ca6bfa277f3965f1c5d74de17cd2983769cc46bb
Travis DeMint <tdemint@ufl.edu>
2017-09-19 18:37:04 -0400

    testing git

--- Calculator.js
+++ Calculator.js
@@ -11,6 +11,7 @@ function subtract(){
 }
 
 function multiply(){
+	// Travis was here
 	var v1=parseInt(document.getElementById("input1").value);
 	var v2=parseInt(document.getElementById("input2").value);
 	alert("Result= " + (v1*v2).toString());
