f49fec818d71685d7ab94b7065d49bcfb69a1c72
Your Name <anthonybrugal@yahoo.com>
2017-09-19 09:12:37 -0400

    Added divide button and function

--- Calculator.js
+++ Calculator.js
@@ -6,20 +6,23 @@ function add(){
 
 function subtract(){
 	//Implement this
-	
-	
+
+
 }
 
 function multiply(){
 	//Implement this
-	
-	
+
+
 }
 
 function divide(){
 	//Implement this
-	
-	
-	
+
+		var v1=parseInt(document.getElementById("input1").value);
+		var v2=parseInt(document.getElementById("input2").value);
+		alert("Result= " + (v1/v2).toString());
+
+
+
 }
-		
