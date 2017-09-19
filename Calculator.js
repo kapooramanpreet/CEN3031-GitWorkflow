298e4df337a4952199da5c944980fab7662382d6
Prakash Pudhucode <pudhucodep@gmail.com>
2017-09-19 13:31:27 -0400

    added subtraction feature

--- Calculator.js
+++ Calculator.js
@@ -6,7 +6,9 @@ function add(){
 
 function subtract(){
 	//Implement this
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1-v2).toString());
 	
 }
 
