e3b97bfa6ae887af9ba610d573eda98c5415338f
Michael Smith <michaeljsmith8@gmail.com>
2017-09-19 13:34:48 -0400

    added multiplication button and function

--- Calculator.js
+++ Calculator.js
@@ -6,20 +6,21 @@ function add(){
 
 function subtract(){
 	//Implement this
-	
-	
+
+
 }
 
 function multiply(){
 	//Implement this
-	
-	
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1*v2).toString());
+
 }
 
 function divide(){
 	//Implement this
-	
-	
-	
+
+
+
 }
-		
