e19254266db2b7f96d831d3cf591fc058ecf7979
Stanley Celestin <stanleycelestin1@gmail.comgit config --global user.namegit config --global user.email stanleycelestin1@gmail.com>
2017-09-22 13:15:51 -0400

    Testing pull request

--- Calculator.js
+++ Calculator.js
@@ -1,4 +1,5 @@
 function add(){
+	// Change
 	var v1=parseInt(document.getElementById("input1").value);
 	var v2=parseInt(document.getElementById("input2").value);
 	alert("Result= " + (v1+v2).toString());
