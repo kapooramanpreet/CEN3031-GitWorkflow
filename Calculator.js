ac24bd762706c039a07802040b53c40c5697144a
Connor Elkin <rpantone@ufl.edu>
2017-09-22 23:06:07 -0400

    Added the authors

--- Calculator.js
+++ Calculator.js
@@ -6,7 +6,7 @@ function add(){
 }
 
 function subtract(){
-	//Implement this
+	//Implement this by Jose, Prakash, and Ross
 	var v1=parseInt(document.getElementById("input1").value);
 	var v2=parseInt(document.getElementById("input2").value);
 	alert("Result= " + (v1-v2).toString());
